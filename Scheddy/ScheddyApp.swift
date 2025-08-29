706c4881fb50e2007f9927771a7cf00f69c5c2c9
angelica1412 <114383532+angelica1412@users.noreply.github.com>
2025-08-29 09:08:56 +0700

    Initial Commit

---
+++ Scheddy/ScheddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheddyApp.swift
+//  Scheddy
+//
+//  Created by Maria Angelica Vinesytha Chandrawan on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ScheddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
