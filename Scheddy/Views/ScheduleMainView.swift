ddd9e1c0a8f0be6b82b054e1fefa22362d2f4a38
Oxqlion <rafinaya1127@gmail.com>
2025-09-03 08:36:53 +0700

    feat: added schedul main view, schedule daily view, and schedule monthly view

---
+++ Scheddy/Views/ScheduleMainView.swift
@@ -0,0 +1,19 @@
+//
+//  ScheduleMainView.swift
+//  Scheddy
+//
+//  Created by Rafi Abhista  on 03/09/25.
+//
+
+import SwiftUI
+
+struct ScheduleMainView: View {
+    var body: some View {
+        Text("Ini schedule main view bang")
+        Text("Nanti masukin daily view sama monthly view di sini")
+    }
+}
+
+#Preview {
+    ScheduleMainView()
+}
