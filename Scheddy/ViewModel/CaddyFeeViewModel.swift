29fb0d05601cb8687dfcb766ac9c7cbc1634194b
wamalias <wardamalias@gmail.com>
2025-09-12 11:55:25 +0700

    feat: caddy fee view model

---
+++ Scheddy/ViewModel/CaddyFeeViewModel.swift
@@ -0,0 +1,89 @@
+//
+//  CaddyFeeViewModel.swift
+//  Scheddy
+//
+//  Created by Wardatul Amalia Safitri on 12/09/25.
+//
+
+import Foundation
+
+//@Observable
+//class CaddyFeeViewModel {
+//    var caddyFees: [CaddyFeeData] = []
+//    var isLoading = false
+//    var errorMessage: String?
+//
+//    private let service: CaddyFeeService
+//
+//    init(service: CaddyFeeService = CaddyFeeService()) {
+//        self.service = service
+//    }
+//
+//    func load(monthNumber: Int) async {
+//        isLoading = true
+//        errorMessage = nil
+//        defer { isLoading = false }
+//
+//        do {
+//            let caddyFeeData = try await service.fetchCaddyFee(month: monthNumber)
+//            self.caddyFees = caddyFeeData
+//        } catch {
+//            errorMessage = "Gagal memuat caddy fee: \(error.localizedDescription)"
+//            print(errorMessage!)
+//        }
+//    }
+//}
+
+@Observable
+class CaddyFeeViewModel {
+    var isLoading = false
+    var errorMessage: String?
+    var caddyFees: [CaddyFeeData] = []
+
+    private let service: CaddyFeeService
+    
+    init(service: CaddyFeeService = CaddyFeeService()) {
+        self.service = service
+    }
+    
+    func load(monthNumber: Int) async {
+        isLoading = true
+        errorMessage = nil
+        defer { isLoading = false }
+        
+        do {
+            let response = try await service.fetchCaddyFee(month: monthNumber)
+            caddyFees = response
+        } catch {
+            errorMessage = "Gagal memuat caddy fee: \(error.localizedDescription)"
+            print(errorMessage!)
+        }
+    }
+    
+    // Dummy data for Preview
+    static let dummy: [CaddyFeeData] = [
+        CaddyFeeData(
+            id_group: "00386de9-eab2-46f2-9def-cdbaea758454",
+            group_name: "Group 1",
+            caddy_group_type: "casual",
+            total_group_fee: 387000,
+            caddies: [
+                Caddies(id: "00e401da-e8e2-401c-a563-0a445c6af697",
+                        name: "Dewi Purnama",
+                        caddy_type: 1,
+                        total_holes: 18,
+                        total_fee: 129000),
+                Caddies(id: "3cab0301-1629-42b3-9840-d1c2255501de",
+                        name: "Maya Firmansyah",
+                        caddy_type: 1,
+                        total_holes: 9,
+                        total_fee: 64500),
+                Caddies(id: "93dd032c-c5b5-4b35-9029-487e5b1476ec",
+                        name: "Wahyu Haryanto",
+                        caddy_type: 1,
+                        total_holes: 27,
+                        total_fee: 193500)
+            ]
+        )
+    ]
+}
--- Scheddy/Views/FeeMainView.swift
+++ Scheddy/Views/FeeMainView.swift
@@ -8,7 +8,69 @@
 import SwiftUI
 
 struct FeeMainView: View {
-    var body: some View {
-        Text("FeeMainView")
+    @State var viewModel = CaddyFeeViewModel()
+    @State private var draggedGroup: CaddyFeeData? = nil
+    
+    var groupedGroup: [String: [CaddyFeeData]] {
+        Dictionary(
+            grouping: viewModel.caddyFees,
+            by: { $0.caddy_group_type }
+        )
     }
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                if viewModel.isLoading {
+                    ProgressView("Loading...")
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(12)
+//                } else if let error = viewModel.errorMessage {
+//                    Text("❌ \(error)")
+//                        .foregroundColor(.red)
+                } else {
+                    ForEach(groupedGroup.keys.sorted(), id: \.self) { groupType in
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("Group Type: \(groupType.capitalized)")
+                                .font(.headline)
+                                .foregroundColor(.black)
+                                .padding(.leading, 10)
+                            
+                            HStack {
+                                Rectangle()
+                                    .fill(Color.teal.opacity(0.7))
+                                    .frame(width: 4)
+                                    .frame(maxHeight: .infinity, alignment: .top)
+                                    .padding(.trailing, 8)
+                                
+                                VStack {
+                                    ForEach(groupedGroup[groupType] ?? [], id: \.id_group) { caddyGroup in
+                                        AccordionGroupView(
+                                            title: "\(caddyGroup.group_name) | Total Rp \(caddyGroup.total_group_fee)",
+                                            caddies: caddyGroup.caddies.map {
+                                                "\($0.name) - Holes: \($0.total_holes), Fee: Rp \($0.total_fee)"
+                                            },
+                                        )
+                                    }
+                                }
+                            }
+                            .fixedSize(horizontal: false, vertical: true)
+                        }
+                    }
+                    .padding()
+                    .cornerRadius(10)
+                    
+                    Spacer()
+                }
+            }
+        }
+        .onAppear {
+            viewModel.caddyFees = CaddyFeeViewModel.dummy // load dummy data
+        }
+    }
+}
+
+#Preview {
+    FeeMainView()
 }
